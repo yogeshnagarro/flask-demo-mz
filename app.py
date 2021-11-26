1f86e669d0e696421a0aaac6bfd906317ab664ce
yogeshnagarro <yogesh.sharma3817@gmail.com>
2021-11-27 00:53:59 +0530

    Updated data model and UI changes

--- app.py
+++ app.py
@@ -1,45 +1,63 @@
 import os
 from flask import Flask, render_template, url_for, json, request
-from datetime import datetime
+import urllib.request, numpy, pickle
 
 app = Flask(__name__)
 companyName = "Green Energy Forcast"
 
-@app.route("/")
-def index():
-    return "Connected ...."
 
-@app.route("/home", methods = ['POST', 'GET'])
+@app.route("/", methods = ['POST', 'GET'])
 def home():
-    form_data = {"Lat" : request.form.get("Latitude"),
-    "Log":request.form.get("Longitude")}
-    if request.method == 'POST':
-      print("form data: ", form_data.get("Lat"));
+    form_data = {"Lat" : request.form.get("Latitude")}
+    data = {}
+    Lat = 0
+    Long = 0
+    city = ''
+    if form_data.get('Lat'):
+        ll = form_data.get('Lat').split(',')
+        Lat = float(ll[0])
+        Long = float(ll[1])
+        if request.method == 'POST':
+          data= get_data(Lat,Long)
+          
+          if data['city']:
+              city = data['city']['name']
+          data = data["list"]
+
+          wpf_model = pickle.load(open('wpf.pkl','rb'))
+          for j in data:
+            print(j['wind']['deg'],j['wind']['speed'])
+          
+          for x in data:
+              arrm = numpy.array([[x['wind']['deg'],x['wind']['speed']]])
+              prdt = wpf_model.predict(arrm)
+              a = float(prdt)
+              x['power_avg']= round(a,2)
     
-    data = json.load(
-        open(os.path.join(app.static_folder, 'data/wind-data.json')))
+    
+    # data = json.load(
+    #     open(os.path.join(app.static_folder, 'data/wind-data.json')))
+    
     columns = [
         {
-            "field": "id",  # which is the field's name of data key
-            "title": "Row ID",  # display as the table header's name
-            "sortable": True,
-        }, {
-            "field": "date-time",  # which is the field's name of data key
+            "field": "dt_txt",  # which is the field's name of data key
             "title": "Date / Time",  # display as the table header's name
             "sortable": True,
         },
         {
-            "field": "wind-speed",  # which is the field's name of data key
+            "field": "wind.speed",  # which is the field's name of data key
             # display as the table header's name
             "title": "Wind Speed (in m/s)",
             "sortable": True,
-        }, {
-            "field": "wind-direction",  # which is the field's name of data key
+        },
+        {
+            "field": "wind.deg",  # which is the field's name of data key
             # display as the table header's name
-            "title": "Wind Direction(*)",
+            "title": "Wind Direction(&deg;)",
             "sortable": True,
-        }, {
-            "field": "power-avg",  # which is the field's name of data key
+        },
+        {
+            "field": "power_avg",  # which is the field's name of data key
             "title": "Power Avg",  # display as the table header's name
             "sortable": True,
         }
@@ -49,13 +67,20 @@ def home():
         "data-table.html",
         users=data,
         columns=columns,
-        companyName=companyName
+        companyName=companyName,
+        lat = Lat,
+        long = Long,
+        city_ = city
     )
 
 
 @app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+def get_data(Latitude = 0,Longitude=0):
+    url = "http://localhost:3800/forecast/data?lat={}&lon={}&appid=37d73035b107a29659dac1d0276d4d75".format(Latitude,Longitude)
+    print(url)
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    return json.loads(data)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True,host='0.0.0.0')
